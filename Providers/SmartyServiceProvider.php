496759f552c2756f6bfce75ae14f41e2c458da67
marco76tv <marco.sottana@gmail.com>
2022-10-26 09:13:34 +0200

    up

--- Providers/SmartyServiceProvider.php
+++ Providers/SmartyServiceProvider.php
@@ -7,20 +7,17 @@ namespace Modules\Smarty\Providers;
 use Modules\Xot\Providers\XotBaseServiceProvider;
 use Modules\Xot\Services\BladeService;
 
-class SmartyServiceProvider extends XotBaseServiceProvider
-{
+class SmartyServiceProvider extends XotBaseServiceProvider {
     protected string $module_dir = __DIR__;
 
     protected string $module_ns = __NAMESPACE__;
 
     public string $module_name = 'smarty';
 
-    public function bootCallback(): void
-    {
+    public function bootCallback(): void {
         BladeService::registerComponents($this->module_dir.'/../View/Components', 'Modules\\Smarty');
     }
 
-    public function registerCallback(): void
-    {
+    public function registerCallback(): void {
     }
 }
