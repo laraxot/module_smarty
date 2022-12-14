483bbf8ffbc963696c9095d39c63eb9ef7d46b3d
Retype GitHub Action <retype@object.net>
2022-12-14 09:12:21 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('smarty::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('smarty.name') !!}
+    </p>
+@endsection
