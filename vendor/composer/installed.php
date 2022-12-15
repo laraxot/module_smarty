798261a9d31d23e7aea1ce7b6bb8510435344d71
marco76tv <marco.sottana@gmail.com>
2022-12-15 09:14:57 +0100

    up

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nwidart/smarty',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '13a11f58f55d04067e0e160a9dcbd0db98485010',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nwidart/smarty' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '13a11f58f55d04067e0e160a9dcbd0db98485010',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
